507cf158ee194d2d064222d13444254fa9da40ea
unknown <g.hitoshi.yokogawa@gmail.com>
2020-06-25 10:24:33 -0300

    Desafios finalizados.

--- Program.cs
+++ Program.cs
@@ -6,7 +6,16 @@ namespace Recursividade
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Sequência fibonacci = o próximo número é sempre a soma dos dois anteriores;
+            Recursividade fibao = new Recursividade();
+            //fibao.GerarValorFibonacci(0,1,30);
+
+            Recursividade fatorial = new Recursividade();
+            Console.WriteLine(fatorial.GerarFatorial(14));
+
+
+
+
         }
     }
 }
--- Recursividade.cs
+++ Recursividade.cs
@@ -1,7 +1,30 @@
-namespace .
+using System;
+
+namespace Recursividade
 {
     public class Recursividade
     {
-        
+        public void GerarValorFibonacci(int num1, int num2, int vezes){
+
+            if(vezes > 0){
+
+                int soma = num1 + num2;
+                Console.WriteLine(num1);
+                GerarValorFibonacci(num2, soma, vezes -1);
+            }
+        }
+
+        public int GerarFatorial(int numero){
+
+            if(numero == 1){
+                return 1;
+            }else{
+
+                return numero * GerarFatorial(numero - 1); 
+
+            }
+
+        }
+
     }
 }
